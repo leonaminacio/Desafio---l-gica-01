152b1a90c2a83a7d66030a7744e1eefd1f427598
leonaminacio <leonamalves46@gmail.com>
2021-05-27 02:17:35 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let nome = "Leonam"
+let ano = 2021
+let idade = 19
+let souHumano = true
+let numero1 = 21
+let numero2 = 32
+console.log("Meu nome é", nome)
+console.log("Meu ano de nascimento é",ano - idade)
+console.log(souHumano)
+console.log(numero1 + numero2)
